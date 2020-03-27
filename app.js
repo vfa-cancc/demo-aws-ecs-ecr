7d424614a9dc4a4d9892c19486841d245a9da390
cucongcan <cancc@vitalify.asia>
2020-03-27 12:44:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log("incomming http request...");;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Wellcome Boss v9696');
+  res.end('Demo v1');
 }).listen(3001);
 console.log("Server started!");;
