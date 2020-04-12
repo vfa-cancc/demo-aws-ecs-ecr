58f5c8019c9de8eb8c28539df7b8633a581e73d9
cucongcan <cancc@vitalify.asia>
2020-04-12 13:25:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   console.log("incomming http request...");;
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Test deploy ECS-EC2 v11');
+  res.end('Test deploy ECS-EC2 v12');
 }).listen(3001);
 console.log("Server started!");;
